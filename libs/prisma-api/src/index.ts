9b447d3f7b1eb5df68cbf6851bd0c54a33e7520c
mdwitr0 <mdwit0r@gmail.com>
2022-06-23 06:10:35 +0200

    Exported prisma api

--- libs/prisma-api/src/index.ts
+++ libs/prisma-api/src/index.ts
@@ -1,3 +1,1 @@
 export * from './lib/prisma-api.module';
-export * from './lib/prisma-api.service';
-export * from '../generated/prisma-client/index';
--- libs/prisma-api/src/lib/prisma-api.module.ts
+++ libs/prisma-api/src/lib/prisma-api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaApiService } from './prisma-api.service';
 
 @Module({
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PrismaApiService],
+  exports: [PrismaApiService],
 })
 export class PrismaApiModule {}
