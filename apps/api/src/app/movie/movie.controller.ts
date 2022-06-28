1fdf3117ff90c8b95e9f766fe2dbc425f71a4575
mdwitr0 <mdwit0r@gmail.com>
2022-06-28 21:45:38 +0200

    feat: add findAll & findOne endpoints

--- apps/api/src/app/movie/movie.controller.ts
+++ apps/api/src/app/movie/movie.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query, Get, Param } from '@nestjs/common';
 import { MovieService } from './movie.service';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
+
+  @Get()
+  async findAll(@Query() query: any) {
+    return this.movieService.findAll(query);
+  }
+
+  @Get(':kpId')
+  async findOne(@Param('kpId') kpId: number) {
+    return this.movieService.findOne(kpId);
+  }
 }
--- apps/api/src/app/movie/movie.service.ts
+++ apps/api/src/app/movie/movie.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaApiService } from '@kinopoiskdev/prisma-api';
+import { FindMovieDto } from './dto/find-movie.dto';
 
 @Injectable()
-export class MovieService {}
+export class MovieService {
+  constructor(private readonly prisma: PrismaApiService) {}
+
+  async findAll(query: any): Promise<FindMovieDto[]> {
+    return this.prisma.movie.findMany({
+      where: {
+        ...query,
+      },
+    });
+  }
+
+  async findOne(kpId: number): Promise<FindMovieDto> {
+    return this.prisma.movie.findUnique({
+      where: {
+        kpId,
+      },
+    });
+  }
+}
